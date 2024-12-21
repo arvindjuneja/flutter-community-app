e27fd1c9ffe073eb7795e48eae447a1c5e74e349
Arvind Juneja <arvind@oumm.pl>
2024-12-21 01:35:30 +0100

    Update build configuration

--- build.sh
+++ build.sh
@@ -1,18 +1,26 @@
 #!/bin/bash
 set -ex
 
+echo "Current directory: $(pwd)"
+echo "Environment variables:"
+env
+
 echo "Setting up environment..."
 FLUTTER_VERSION=${FLUTTER_VERSION:-"3.27.1"}
 
+echo "Creating Flutter home directory..."
+FLUTTER_HOME="$HOME/flutter"
+mkdir -p "$FLUTTER_HOME"
+
 echo "Downloading Flutter SDK..."
-mkdir -p $HOME/flutter
-wget -q https://storage.googleapis.com/flutter_infra_release/releases/stable/linux/flutter_linux_${FLUTTER_VERSION}-stable.tar.xz
-tar xf flutter_linux_${FLUTTER_VERSION}-stable.tar.xz -C $HOME
+wget -q "https://storage.googleapis.com/flutter_infra_release/releases/stable/linux/flutter_linux_${FLUTTER_VERSION}-stable.tar.xz"
+tar xf "flutter_linux_${FLUTTER_VERSION}-stable.tar.xz" -C "$HOME"
 
 echo "Adding Flutter to PATH..."
-export PATH="$HOME/flutter/bin:$PATH"
+export PATH="$FLUTTER_HOME/bin:$PATH"
 
 echo "Verifying Flutter installation..."
+which flutter
 flutter --version
 
 echo "Enabling web support..."
@@ -24,3 +32,6 @@ flutter pub get
 
 echo "Building web app..."
 flutter build web --release
+
+echo "Build completed. Contents of build/web:"
+ls -la build/web
